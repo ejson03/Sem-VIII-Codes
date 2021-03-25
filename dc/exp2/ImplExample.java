c7f8c2c3884216f64aab4653d93f03c55c1ed075
ejson03 <elvisejsondsouza57@gmail.com>
2021-03-25 14:42:48 +0530

    Added proper exp 2 dc

---
+++ dc/exp2/ImplExample.java
@@ -0,0 +1,7 @@
+public class ImplExample implements Hello {  
+   
+   // Implementing the interface method 
+   public void printMsg(String msg) {  
+      System.out.println(msg);  
+   }  
+}
---
+++ dc/exp2/README.md
@@ -0,0 +1,7 @@
+
+## Commands
+1. `javac *.java`  
+Run 2,3,4 in different CMDs
+2. `start rmiregistry`
+3. `java Server`
+4. `java Client`
